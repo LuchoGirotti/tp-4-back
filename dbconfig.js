5d9207b22e7758ef41f6bbe86abca72c860e6b03
Martooooooo <166547016+Martooooooo@users.noreply.github.com>
2025-10-28 08:36:56 -0300

    empezado

--- dbconfig.js
+++ dbconfig.js
@@ -1,13 +1,27 @@
-export const config = {
+import { Sequelize } from 'sequelize';
+export const sequelize = new Sequelize({
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
-    user: process.env.PGUSER,
+    username: process.env.PGUSER,
     password: process.env.PGPASSWORD,
     port: 5432,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    logging: false 
+});
+try {
+    await sequelize.authenticate();
+    console.log('✅ Connection to database has been established successfully.');
+} catch (error) {
+    console.error('❌ Unable to connect to the database:', error);
 }
+
+
 
 
  
